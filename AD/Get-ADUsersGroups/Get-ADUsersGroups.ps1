2ea762983f7379726f74baa2f24efe89862d8039
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-06 11:47:21 +1300

    Update Get-ADUsersGroups.ps1

--- AD/Get-ADUsersGroups/Get-ADUsersGroups.ps1
+++ AD/Get-ADUsersGroups/Get-ADUsersGroups.ps1
@@ -6,11 +6,14 @@
         [string]$Account
     )
     
-    # Get a User's Groups By Name
+    # Get a User's Groups By User Name
     # $Account = "JohnS"
 
-    
-    $Account = Read-Host "Username?"
+     # If $Account is null use Read-Host
+     if( !($null -eq $Account ) ){
+        
+        $Account = Read-Host "Username?" ; $Account
+    }
 
     # Get a User's Groups By SAM Account Name
     ( (Get-ADuser -Identity $Account -Properties memberof).memberof | Get-ADGroup | Select-Object Name | Sort-Object name).Name
