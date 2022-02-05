bda11b98c219971e320d0d1021cf49b70bc1579f
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-06 11:38:50 +1300

    Create Get-ADUsersGroupsByUserTitle.ps1

---
+++ AD/Get-ADUsersGroups/Get-ADUsersGroupsByUserTitle.ps1
@@ -0,0 +1,15 @@
+﻿function Get-ADUsersGroupsByUserTitle{
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$Title
+    )
+    
+    # Get a User's Groups By Name
+    # $Title = "Fines Administrator"
+
+    $Title = Read-Host "Title of User?"
+    
+    ( (Get-ADUser -Filter {Title -eq $Title} -Properties memberof,title,whenCreated | Sort-Object whenCreated).memberof | Get-ADGroup | Select-Object Name ).Name
+}
