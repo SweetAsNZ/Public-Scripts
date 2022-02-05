6150f7197c0d12297a5404e314721d7334850a07
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-06 11:14:36 +1300

    Create Get-ADUsersWithoutMobile.ps1

---
+++ AD/Get-ADUsersWithoutMobile/Get-ADUsersWithoutMobile.ps1
@@ -0,0 +1,6 @@
+﻿# ADUsers With No Mobile
+$Output = "C:\Temp\ADUsers_WithNoMobile.csv"
+
+Get-ADUser -Filter * -Properties * | Where-Object {$_.Mobile -eq $NUll} |
+ Select-Object name,sAMAccountName,givenName,sn,mail,employeeType,company,Department,@{n='OU';e={$_.distinguishedname -replace "CN=$($_.cn),",""}} |
+  Sort-Object Name | Export-Csv -NoTypeInformation $Output
