cefc6c3a0367e86d06f772f25b978850e6f2742f
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-06 12:10:36 +1300

    Create Get-ADUniqeStreetAddress.ps1

---
+++ AD/Get-ADUniqeStreetAddress/Get-ADUniqeStreetAddress.ps1
@@ -0,0 +1,10 @@
+﻿function Get-ADUniqeStreetAddress{}
+
+    # Unique Street Addresses
+    $Addresses = (Get-ADUser -Filter * -Properties Enabled,Name,StreetAddress | Where-Object {($_.Enabled -eq $true) -and ($_.StreetAddress -ne $null)}).StreetAddress 
+    $Addresses | Sort-Object | Get-Unique
+
+    # Get User With Address
+    $Address = ""
+    Get-ADUser -Filter * -Properties Name,StreetAddress| Where-Object {($_.StreetAddress -eq $Address)}
+}
