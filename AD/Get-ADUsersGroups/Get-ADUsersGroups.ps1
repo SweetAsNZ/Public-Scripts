24698bfebe96a8b5a2c0f18373940903ec6c7557
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-06 11:38:35 +1300

    Create Get-ADUsersGroups.ps1

---
+++ AD/Get-ADUsersGroups/Get-ADUsersGroups.ps1
@@ -0,0 +1,17 @@
+﻿function Get-ADUsersGroups{
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$Account
+    )
+    
+    # Get a User's Groups By Name
+    # $Account = "JohnS"
+
+    
+    $Account = Read-Host "Username?"
+
+    # Get a User's Groups By SAM Account Name
+    ( (Get-ADuser -Identity $Account -Properties memberof).memberof | Get-ADGroup | Select-Object Name | Sort-Object name).Name
+}
