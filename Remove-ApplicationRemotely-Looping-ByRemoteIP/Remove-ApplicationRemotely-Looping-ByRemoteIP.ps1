090acae86f470d510c52780cadfb6b41d723695c
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-07 09:07:46 +1300

    Create Remove-ApplicationRemotely-Looping-ByRemoteIP.ps1

---
+++ Remove-ApplicationRemotely-Looping-ByRemoteIP/Remove-ApplicationRemotely-Looping-ByRemoteIP.ps1
@@ -0,0 +1,38 @@
+﻿function Remove-ApplicationRemotely-Looping-ByRemoteIP{
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$AppID,
+        [Parameter(Mandatory=$true)]
+        [string]$ServerIP
+    )
+    ######################################
+    ########   TREND SERVER   ############
+
+    #$AppID = ''
+    $Collection = (Get-NetTCPConnection -State Established | Where-Object {$_.LocalPort -eq 80}).RemoteAddress
+
+    foreach ($Item in $Collection)
+    {
+        Invoke-Command -ComputerName $Item -ArgumentList $AppID -ScriptBlock {
+        
+            param(
+                [Parameter(Mandatory)]
+                [ValidateNotNullOrEmpty()]
+                $AppID
+            )
+        
+            MSIEXEC.exe /x $AppID /qn 
+        }
+    }
+
+    <#
+    ##############################################
+    ########   RUN FROM ALL CLIENTS   ############
+    
+    $ServerIP = '10.1x.1x.1x'
+    $TESTPC   = Get-NetTCPConnection -State Established | Where-Object {($_.LocalPort -eq 5985) -and ($_.RemoteAddress -eq $ServerIP)}
+    $TESTPC 
+    #>
+}
