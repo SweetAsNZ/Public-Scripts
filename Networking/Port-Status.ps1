c311fd0eef71068822f7ac7bb75ab813a4168953
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-07 18:50:51 +1300

    Update Port-Status.ps1

--- Networking/Port-Status.ps1
+++ Networking/Port-Status.ps1
@@ -1,13 +1,48 @@
 function Get-PortStatus {
+<#
+.SYNOPSIS
+Gets the status of a port
+
+If you recieve this: Get-NetTCPConnection : No MSFT_NetTCPConnection objects found with property 'LocalPort' equal to* it is  a 'No'
+
+.DESCRIPTION
+
+'# WIP' Means it is still a Work In Progress
+
+###########################################
+##  Author:      Tim West (BSc.)         ##
+##  Company:     Sweet As Chocolate Ltd  ##
+###########################################
+
+
+.PARAMETER Name
+N/A
+.PARAMETER Extension
+N/A
+.INPUTS
+N/A
+.OUTPUTS
+Screen
+.EXAMPLE
+Get-PortStatus -Port 80
+.LINK
+
+#>
     
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)] 
         [ValidateNotNullOrEmpty()] 
-        [Alias("Port")] 
-        [string]$Port 
+        #[Alias("Port")] 
+        [int]$Port 
     )
     
     Get-NetTCPConnection -LocalPort $Port
+
 }
-Get-PortStatus -Port 443
+
+
+
+
+
+    
