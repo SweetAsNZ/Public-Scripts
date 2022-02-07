0b3bb5628247d4bdf89d595f75e174057346f0a5
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-07 17:05:33 +1300

    Update New-FileCopyBasedOnStringInFile.ps1

--- Files-And-Folders/New-FileCopyBasedOnStringInFile.ps1
+++ Files-And-Folders/New-FileCopyBasedOnStringInFile.ps1
@@ -1,13 +1,46 @@
 ﻿function New-FileCopyBasedOnStringInFile{
+<#
+.SYNOPSIS
+Copies Files The Contents Of Which Are a String $Str from $Source to $Dest
+
+.DESCRIPTION
+Script to copy the most recent file containing a certain string from one folder to another folder on a Windows machine.
+
+'# WIP' Means it is still a Work In Progress
+
+###########################################
+##  Author:      Tim West                ##
+##  Company:     Sweet As Chocolate Ltd  ##
+###########################################
+
+
+.PARAMETER Name
+[string]$Source
+[string]$Dest
+[string]$FileType
+[string]$Str
+.PARAMETER Extension
+N/A
+.INPUTS
+N/A
+.OUTPUTS
+Log File
+.EXAMPLE
+New-FileCopyBasedOnStringInFile -Source C:\Temp -Dest C:\Temp\Folder2 -Filetype TXT -Str cat
+.LINK
+
+#>
     
+
+
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true, 
-        ValueFromPipelineByPropertyName=$true)] 
+        ValueFromPipelineByPropertyName=$true,HelpMessage="Source Path")] 
         [ValidateNotNullOrEmpty()] 
         [string]$Source,
         [Parameter(Mandatory=$true, 
-        ValueFromPipelineByPropertyName=$true)] 
+        ValueFromPipelineByPropertyName=$true,HelpMessage="Destination Path")] 
         [ValidateNotNullOrEmpty()] 
         [string]$Dest,
         [Parameter(Mandatory=$true, 
@@ -15,14 +48,14 @@
         [ValidateNotNullOrEmpty()] 
         [string]$FileType,
         [Parameter(Mandatory=$true, 
-        ValueFromPipelineByPropertyName=$true)] 
+        ValueFromPipelineByPropertyName=$true,HelpMessage="String to Search For In The Files")] 
         [ValidateNotNullOrEmpty()] 
         [string]$Str
     )
 
 
-# Script to copy the most recent file containing a certain string from one folder to another folder on a Windows machine.
 
-Copy-Item (Get-ChildItem -Path $Source -File -Name *.$FileType | Get-Content | Where-Object {$_ -like "*$Str*"} | Select-Object -First 1).PSPath -Destination $Dest
+    Copy-Item ( Get-ChildItem -Path $Source -File -Name ("*."+$FileType) | Get-Content | Where-Object {$_ -like "*$Str*"} ).PSPath -Destination $Dest -Verbose
 
 }
+
