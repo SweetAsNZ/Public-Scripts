06ed192efc3b0a004744fbb8250b19fb54498851
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-07 16:55:39 +1300

    Delete File_Copy_Based_On_Text_String_In_File.ps1

--- Files-And-Folders/File_Copy_Based_On_Text_String_In_File.ps1
+++
@@ -1,4 +0,0 @@
-﻿# Script to copy the most recent file containing a certain string from one folder to another folder on a Windows machine.
-
-Copy-Item (Get-ChildItem -Path C:\Temp -File -Name *.txt | Get-Content | Where {$_ -like "*cat*"} | Select -First 1).PSPath -Destination C:\Temp\Folder2
-
