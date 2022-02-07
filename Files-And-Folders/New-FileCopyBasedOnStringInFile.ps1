e2ee555b4ec0549c260c18e21070eabff0cf5b3e
SweetAsChocolate <57114559+SweetAsNZ@users.noreply.github.com>
2022-02-07 16:55:36 +1300

    Create New-FileCopyBasedOnStringInFile.ps1

---
+++ Files-And-Folders/New-FileCopyBasedOnStringInFile.ps1
@@ -0,0 +1,28 @@
+﻿function New-FileCopyBasedOnStringInFile{
+    
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true, 
+        ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$Source,
+        [Parameter(Mandatory=$true, 
+        ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$Dest,
+        [Parameter(Mandatory=$true, 
+        ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$FileType,
+        [Parameter(Mandatory=$true, 
+        ValueFromPipelineByPropertyName=$true)] 
+        [ValidateNotNullOrEmpty()] 
+        [string]$Str
+    )
+
+
+# Script to copy the most recent file containing a certain string from one folder to another folder on a Windows machine.
+
+Copy-Item (Get-ChildItem -Path $Source -File -Name *.$FileType | Get-Content | Where-Object {$_ -like "*$Str*"} | Select-Object -First 1).PSPath -Destination $Dest
+
+}
