9398bb3ba086537938a156eac4f59204c208792a
Tim West <57114559+SweetAsNZ@users.noreply.github.com>
2023-11-14 10:36:20 +1300

    Add files via upload

---
+++ Clear-MyVariables.ps1
@@ -0,0 +1,19 @@
+﻿function ClearMyVariables
+{
+    Get-ChildItem -Recurse C:\SCRIPTS -Include *.ps1 | 
+    ForEach-Object {
+        $content = Get-Content $_.FullName
+        $variables = $content | Select-String -Pattern '\$\w+' -AllMatches | 
+        ForEach-Object { $_.Matches } | 
+        ForEach-Object { $_.Value }
+        $variables | Sort | Get-Unique | ForEach-Object {
+            try {
+                
+                Remove-Variable -Name $_.Substring(1) -ErrorAction Stop #-WhatIf
+            } catch {
+                #Write-Output "Failed to remove variable $_"
+            }
+        }
+    }
+    
+}
